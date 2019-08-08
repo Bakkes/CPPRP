d4620b5c1a3a61c377a91d8dd358ce247490dd54
Chris <chris.m@live.nl>
2019-08-08 19:05:41 +0200

    Removed bash scripts since we have makefile now

--- linux/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm *.o 
-rm ./main
-g++-9 -std=c++17 -Wall -fPIC -c *.cpp -Ofast
-g++-9 -std=c++17 *.o -o main -pthread -lm 
-echo "w0t"
--- linux/cbuild.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm *.o 
-rm ./main
-clang++-6.0 -std=c++17 -Wall -fPIC -c *.cpp -Ofast
-clang++-6.0 -std=c++17 *.o -o main -pthread -lm 
-echo "w0t"
