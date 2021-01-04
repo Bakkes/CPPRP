b49482b614425dc910359d9c2ae7019f3d5eeec7
Bakkes <chris.m@live.nl>
2021-01-04 03:01:13 +0100

    Move outside of .bat again

--- buildwindows.bat
+++ buildwindows.bat
@@ -3,5 +3,3 @@ C:\Python37\python "./scripts/preprocessgameclass.py" "./CPPRP/data/GameClasses.
 C:\Python37\python "./scripts/preprocessnetworkdata.py" "./CPPRP/data/NetworkData.h" consume > "./CPPRP/generated/NetworkDataParsersGenerated.h"
 C:\Python37\python "./scripts/preprocessnetworkdata.py" "./CPPRP/data/NetworkData.h" json > "./CPPRPJSON/GeneratedSerializeFunctions.h"
 MSBuild.exe CPPRP.sln -p:Configuration=JSONBuild -p:Platform=x64
-move "./x64/JSONBuild/CPPRPJSON.exe" "cpprpjson-$NAME-$TRAVIS_TAG.exe"; 
-move "./x64/JSONBuild/CPPRP.lib" "cpprp-$NAME-$TRAVIS_TAG.lib";
